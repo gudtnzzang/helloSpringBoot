4609acba67a206a69f445133862b0d25357fe047
gudtnzzang <hs950823@gmail.com>
2020-06-29 17:07:35 +0900

    Spring Boot practice

--- src/main/java/kr/ac/hansung/cse/repo/ProductRepository.java
+++ src/main/java/kr/ac/hansung/cse/repo/ProductRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.repository.CrudRepository;
 
 import kr.ac.hansung.cse.model.Product;
 
-public interface ProductRepository extends CrudRepository<Product, Integer> { // Long : Customer의 id type
+public interface ProductRepository extends CrudRepository<Product, Integer> {
 	List<Product> findByCategory(String category); // findBy + field name
 }
